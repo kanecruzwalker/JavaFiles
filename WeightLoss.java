0eaf4ade775a619af31ceda773bf4e9bbe3e9be5
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2021-02-04 19:38:43 -0800

    initial pseudo code

--- WeightLoss.java
+++ WeightLoss.java
@@ -1,3 +1,21 @@
 public class WeightLoss {
-    
+    // User starting weight, display table showing what their expected weight will be at the end of each month for the next 6 months if they keep diet. 
+    // Declare Variables
+    // userWeight
+    // dailyCalories
+    // poundsPerMonth
+    // months
+
+
+    // Logic
+    // Input Weight 
+    // Cut Back 500 calories a day
+    // lose 4 pounds a month
+    // for 6 months 
+    // Output
+    // table expected weight at the end of each month
+    // weight - (weight lost this month)
+    // weight lost this month = userWeight - 4 ,
+    // weight = weight lost this month 
+    // weight lost for month 2 = 
 }
