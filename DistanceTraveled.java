9ac4450c8dcec8a167844afb45acce9b56df1da5
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2021-02-04 16:34:25 -0800

    distance calculations made based off of different times

--- DistanceTraveled.java
+++ DistanceTraveled.java
@@ -2,8 +2,31 @@ public class DistanceTraveled {
     public static void main(String[] args)
     {
         int speed;
+        int time1;
+        int time2;
+        int time3;
+        int distance1;
+        int distance2;
+        int distance3;
+
+        // Speed of travel
         speed = 60;
+        
+        // Different time intervals
+        time1 = 5;
+        time2 = 8;
+        time3 = 12;
+
+        // Distance based on time intervals
+        distance1 = speed * time1;
+        distance2 = speed * time2;
+        distance3 = speed * time3;
+        
+        // Output
         System.out.println(speed);
+        System.out.println(distance1);
+        System.out.println(distance2);
+        System.out.println(distance3);
         //Design a program that displays the distance a car will travel if going 
         //60 mph at 5, 8, & 12 hrs.
 
