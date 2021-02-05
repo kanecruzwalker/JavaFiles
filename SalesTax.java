30725939eee2ddfdf0db508dd75c0520d098dc72
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2021-02-04 17:14:04 -0800

    New App, Sales Tax. Pseudo Code & Insanity Check

---
+++ SalesTax.java
@@ -0,0 +1,23 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class SalesTax {
+    public static void main(String [] args)
+    {
+        Scanner keyboard = new Scanner (System.in);
+        BigDecimal purchasePrice;
+
+        System.out.print("What is the price for this purchase?");
+        purchasePrice = keyboard.nextBigDecimal();
+
+        System.out.println("Purchase Price: " + purchasePrice);
+    }
+}
+
+//A program that displays the amount of the purchase, the state sales tax, the county sales tax, the total slaes tax, and the total of the sale 
+// Declare Variables
+// purchasePrice, stateTax, countryTax, totalSalesTax, saleTotal, purchaseStateTax, purchaseCountyTax
+// Logic
+// ????
+// Output
+// Receipt ????
