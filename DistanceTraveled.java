ff1e00028d0e83499253e2a9f407f4eeb7d57404
Kane Russell Cruz-Walker <kanercruzwalker@gmail.com>
2021-02-04 16:23:36 -0800

    New Program, pseudocode and sanity check

---
+++ DistanceTraveled.java
@@ -0,0 +1,17 @@
+public class DistanceTraveled {
+    public static void main(String[] args)
+    {
+        int speed;
+        speed = 60;
+        System.out.println(speed);
+        //Design a program that displays the distance a car will travel if going 
+        //60 mph at 5, 8, & 12 hrs.
+
+        // Declare Variables
+        // Distance 1, Distance 2, Distance  , Speed , Time 1, Time 2, Time 3
+
+        // Program Logic
+
+        // Outcome
+    }
+}
